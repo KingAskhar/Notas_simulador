29432e0fd9e48a55a7db1f958f05941cdb39e7e5
KingAskhar <dramirez12345@cesde.net>
2026-04-16 21:00:03 -0500

    Formato

--- src/main/java/com/example/application/NotaPractica.java
+++ src/main/java/com/example/application/NotaPractica.java
@@ -8,17 +8,20 @@ public class NotaPractica extends Notas implements Aritmetica {
     private double pesoTaller = 0.50;
     private double pesoProyecto = 0.30;
 
-    public NotaPractica(double parcial, double taller, double proyecto){
+    public NotaPractica(double parcial, double taller, double proyecto) {
         super(parcial, taller, proyecto);
     }
+
     @Override
-    public double calcular(double valor){
-        return(getParcial() * pesoParcial) + (getTaller()*pesoTaller)+(getProyecto()*pesoProyecto);
+    public double calcular(double valor) {
+        return (getParcial() * pesoParcial) + (getTaller() * pesoTaller) + (getProyecto() * pesoProyecto);
     }
+
     @Override
     public String nombre() {
         return "Materia practica";
     }
+
     @Override
     public String asignatura() {
         return "JAVA";
--- src/main/java/com/example/application/views/notas/Aritmetica.java
+++ src/main/java/com/example/application/views/notas/Aritmetica.java
@@ -2,9 +2,12 @@ package com.example.application.views.notas;
 
 public interface Aritmetica {
     String nombre();
+
     String asignatura();
+
     double calcular(double valor);
-    default String etiqueta() {    
+
+    default String etiqueta() {
         return "Cálculo de " + nombre();
     }
 }
